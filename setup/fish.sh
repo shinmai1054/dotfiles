b86c1a739402c947d72e5985b9e3be4943fe5784
shin-mba <shin@shin-mai.com>
2021-11-02 21:53:44 +0900

    プラグインを色々インストール

--- setup/fish.sh
+++ setup/fish.sh
@@ -1,4 +1,4 @@
 # fisher
 if ! type fisher > /dev/null 2>&1
-    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher && fisher update
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher oh-my-fish/theme-bobthefish jethrokuan/z jethrokuan/fzf
 end
