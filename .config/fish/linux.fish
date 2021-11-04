4c0ca92d55a0635731e4729a2b594d5471233a7b
shin-mba <shin@shin-mai.com>
2021-11-04 12:16:25 +0900

    FIX bat, fdの問題

--- .config/fish/linux.fish
+++ .config/fish/linux.fish
@@ -1,8 +1,13 @@
 # linux.fish
 
 alias ls 'ls -FX --color=auto'
-alias bat 'batcat'
-alias fd 'fdfind'
 alias i 'sudo apt install -y'
 alias ug 'sudo apt update && sudo apt upgrade -y'
 alias copy 'xsel -bi'
+
+if type "batcat" > /dev/null 2>&1
+    alias bat 'batcat'
+end
+if type "fdfind" > /dev/null 2>&1
+alias fd 'fdfind'
+end
