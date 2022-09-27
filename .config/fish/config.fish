fd2a2e1a574fc7e82a7875033dcbbee3314c890e
shinmai1054 <matsui1054@icloud.com>
2022-09-27 16:45:13 +0900

    color

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -51,10 +51,10 @@ if type "code" > /dev/null 2>&1
    set EDITOR code
    alias v 'code .'
 else
-   set EDITOR emacs
+   set EDITOR 'emacs -nw'
 end
 
-set SUDO_EDITOR emacs
+set SUDO_EDITOR 'emacs -nw'
 
 function es
    emacs /sudo::$argv
@@ -81,38 +81,38 @@ function cd
 end
 
 # color
-set -g fish_color_normal normal
-set -g fish_color_quote ffcc66
-set -g fish_color_command 99cc99
-set -g fish_color_redirection d3d0c8
-set -g fish_color_end cc99cc
-set -g fish_color_error f2777a
-set -g fish_color_param d3d0c8
-set -g fish_color_comment ffcc66
-set -g fish_color_match 6699cc
-set -g fish_color_selection c0c0c0
-set -g fish_color_search_match ffff00
-set -g fish_color_history_current normal
-set -g fish_color_operator 6699cc
-set -g fish_color_escape 66cccc
-set -g fish_color_cwd 008000
-set -g fish_color_valid_path normal
-set -g fish_color_cwd_root 800000
-set -g fish_color_autosuggestion 747369
-set -g fish_color_user 00ff00
-set -g fish_color_host normal
-set -g fish_color_cancel normal
-set -g fish_pager_color_completion normal
-set -g fish_pager_color_description B3A06D yellow
-set -g fish_pager_color_prefix normal --bold --underline
-set -g fish_pager_color_progress brwhite --background=cyan
+# set -g fish_color_normal normal
+# set -g fish_color_quote ffcc66
+# set -g fish_color_command 99cc99
+# set -g fish_color_redirection d3d0c8
+# set -g fish_color_end cc99cc
+# set -g fish_color_error f2777a
+# set -g fish_color_param d3d0c8
+# set -g fish_color_comment ffcc66
+# set -g fish_color_match 6699cc
+# set -g fish_color_selection c0c0c0
+# set -g fish_color_search_match ffff00
+# set -g fish_color_history_current normal
+# set -g fish_color_operator 6699cc
+# set -g fish_color_escape 66cccc
+# set -g fish_color_cwd 008000
+# set -g fish_color_valid_path normal
+# set -g fish_color_cwd_root 800000
+# set -g fish_color_autosuggestion 747369
+# set -g fish_color_user 00ff00
+# set -g fish_color_host normal
+# set -g fish_color_cancel normal
+# set -g fish_pager_color_completion normal
+# set -g fish_pager_color_description B3A06D yellow
+# set -g fish_pager_color_prefix normal --bold --underline
+# set -g fish_pager_color_progress brwhite --background=cyan
 
 # bobthefish
-set -g theme_show_exit_status yes
-set -g theme_title_display_process yes
-set -g theme_project_dir_length 1
-set -g theme_newline_cursor yes
-set -g theme_date_format "+%m/%d %H:%M:%S"
+# set -g theme_show_exit_status yes
+# set -g theme_title_display_process yes
+# set -g theme_project_dir_length 1
+# set -g theme_newline_cursor yes
+# set -g theme_date_format "+%m/%d %H:%M:%S"
 
 # fzf
 set -g FZF_COMPLETE 2
