ac83a41d55570f287ee27553813254cf473250bc
shinmai1054 <matsui1054@icloud.com>
2023-01-30 18:20:17 +0900

    update prompt

--- script/fish.fish
+++ script/fish.fish
@@ -2,6 +2,6 @@
 
 # fisher
 if not type "fisher" > /dev/null 2>&1
-    set packages jorgebucaran/fisher jethrokuan/z jethrokuan/fzf jorgebucaran/autopair.fish dracula/fish jorgebucaran/hydro
+    set packages jorgebucaran/fisher jethrokuan/z jethrokuan/fzf jorgebucaran/autopair.fish dracula/fish shinmai1054/hydro
     curl -sL https://git.io/fisher | source && fisher install $packages
 end
